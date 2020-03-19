25ce665fe9799fc98d2dec9ea90bdd1aad3bb0c3
uu082799 <ymotoba@gmail.com>
2020-03-19 18:19:15 +0900

    ・適当なBottomAppBar実装

---
+++ lib/constant/const_size.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ConstSizes {
+  static const double bottomAppBarIcon = 24.0;
+  static const double bottomAppBarPadding = 28.0;
+  static const double bottomAppBarHeight = kBottomNavigationBarHeight;
+}
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterarchitecturesample/constant/const_colors.dart';
+import 'package:flutterarchitecturesample/constant/const_size.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -10,6 +11,7 @@ class HomePage extends StatelessWidget {
       body: Text(
         'HomePageBody',
       ),
+      bottomNavigationBar: _bottomNavigationBar(),
     );
   }
 
@@ -19,6 +21,45 @@ class HomePage extends StatelessWidget {
       title: Text(
         'サンプルアプリ',
       ),
+    );
+  }
+
+  BottomAppBar _bottomNavigationBar() {
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      child: Container(
+        height: ConstSizes.bottomAppBarHeight,
+        child: _bottomAppBarRow(),
+      ),
+    );
+  }
+
+  Row _bottomAppBarRow() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        _bottomAppBarIcon(Icon(Icons.home), () {
+          debugPrint('press bar icon home');
+        }),
+        _bottomAppBarIcon(Icon(Icons.search), () {
+          debugPrint('press bar icon search');
+        }),
+        _bottomAppBarIcon(Icon(Icons.notifications), () {
+          debugPrint('press bar icon notifications');
+        }),
+        _bottomAppBarIcon(Icon(Icons.list), () {
+          debugPrint('press bar icon list');
+        }),
+      ],
+    );
+  }
+
+  IconButton _bottomAppBarIcon(Icon icon, void Function() onPressed) {
+    return IconButton(
+      iconSize: ConstSizes.bottomAppBarIcon,
+      icon: icon,
+      onPressed: onPressed,
     );
   }
 }
