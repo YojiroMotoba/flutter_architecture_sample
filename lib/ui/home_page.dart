3fe6a1a3403a5a08369a91428607f41c9d50d31a
uu082799 <ymotoba@gmail.com>
2020-03-19 17:48:22 +0900

    ・適当なAppBar実装

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,9 +1,24 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterarchitecturesample/constant/const_colors.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('HomePage');
+    return Scaffold(
+      appBar: _appBar(),
+      body: Text(
+        'HomePageBody',
+      ),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: ConstColors.white,
+      title: Text(
+        'サンプルアプリ',
+      ),
+    );
   }
 }
