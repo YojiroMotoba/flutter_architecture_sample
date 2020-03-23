5d8fdc19c64ce9cdc18e7c3862fd4aac27443447
uu082799 <ymotoba@gmail.com>
2020-03-23 11:19:10 +0900

    ・未使用サイズ定義を削除

--- lib/constant/const_size.dart
+++ lib/constant/const_size.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ConstSizes {
+  // [BottomNavigationBar#iconSize]
   static const double bottomAppBarIcon = 24.0;
-  static const double bottomAppBarPadding = 28.0;
   static const double bottomAppBarHeight = kBottomNavigationBarHeight;
 }
