77516be918ece17c0dd5be5d7cf9985424c130cd
uu082799 <ymotoba@gmail.com>
2020-03-25 10:05:07 +0900

    ・冗長な記述を修正

--- lib/event/event.dart
+++ lib/event/event.dart
@@ -1,7 +1,5 @@
 import 'package:event_bus/event_bus.dart';
 
-EventBus _eventBus = EventBus();
+EventBus _eventBus;
 
-EventBus get eventBus {
-  return _eventBus ??= EventBus();
-}
+EventBus get eventBus => _eventBus ??= EventBus();
