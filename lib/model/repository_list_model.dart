9a590e58aa795e8b54592e67b737f7f04ff305a0
yojiromotoba <caf5a7bebd736d5a82a12724ca6c40d5f024e337>
2020-03-31 09:52:58 +0900

    api呼び出しのとこちょっとだけリファクタ

--- lib/model/repository_list_model.dart
+++ lib/model/repository_list_model.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutterarchitecturesample/event/event.dart';
 import 'package:flutterarchitecturesample/event/repository_search_event.dart';
+import 'package:http/http.dart';
 import 'package:repository/api/github_api.dart';
 import 'package:repository/api/response/repositories.dart';
 
@@ -13,7 +14,7 @@ class RepositoryListModel extends ChangeNotifier {
     _searchSubscription =
         EventBusExt().on<RepositorySearchEvent>().listen((event) {
       _searchWord = event.data.searchWord;
-      search();
+      _search();
     });
   }
 
@@ -23,24 +24,34 @@ class RepositoryListModel extends ChangeNotifier {
 
   set searchWord(String searchWord) {
     _searchWord = searchWord;
-    search();
+    _search();
   }
 
-  void search() {
-    GithubApi.searchRepositories(_searchWord).then((response) {
-      debugPrint('value.statusCode=${response.statusCode}');
-      debugPrint(response.body);
-      final repositories = Repositories.fromJson(response.body);
-      debugPrint('repositories.total_count=${repositories.total_count}');
-      debugPrint('repositories.items.length=${repositories.items.length}');
-      debugPrint('repositories.items[0]=${repositories.items[0].full_name}');
-      debugPrint(
-          'repositories.items[0].owner.avatar_url=${repositories.items[0].owner.avatar_url}');
-    }).catchError((Object error) {
-      debugPrint(error.toString());
-    }).whenComplete(() {
-      notifyListeners();
-    });
+  void _search() {
+    GithubApi.searchRepositories(_searchWord)
+        .then((response) => _searchOnValue(response))
+        .catchError((Object error) => _searchError(error))
+        .whenComplete(() => _searchComplete());
+  }
+
+  void _searchOnValue(Response response) {
+    debugPrint('value.statusCode=${response.statusCode}');
+    debugPrint(response.body);
+    final repositories = Repositories.fromJson(response.body);
+    debugPrint('repositories.total_count=${repositories.total_count}');
+    debugPrint('repositories.items.length=${repositories.items.length}');
+    debugPrint('repositories.items[0]=${repositories.items[0].full_name}');
+    debugPrint(
+        'repositories.items[0].owner.avatar_url=${repositories.items[0].owner.avatar_url}');
+  }
+
+  void _searchError(Object error) {
+    debugPrint(error.toString());
+  }
+
+  void _searchComplete() {
+    debugPrint('_searchComplete');
+    notifyListeners();
   }
 
   @override
