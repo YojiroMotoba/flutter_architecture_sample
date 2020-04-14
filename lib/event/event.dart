fab240376c90983b90ecac2994835a914398d56b
yojiromotoba <caf5a7bebd736d5a82a12724ca6c40d5f024e337>
2020-04-14 10:25:06 +0900

    ・プライベートコンストラクタは名称なしにした

--- lib/event/event.dart
+++ lib/event/event.dart
@@ -1,9 +1,9 @@
 import 'package:event_bus/event_bus.dart';
 
 class EventBusExt extends EventBus {
-  static final EventBusExt _instance = EventBusExt._internal();
+  static final EventBusExt _instance = EventBusExt._();
 
   factory EventBusExt() => _instance;
 
-  EventBusExt._internal();
+  EventBusExt._();
 }
